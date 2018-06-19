fc8500495618e52b210a6f0c162117e36ae28026
Matt <matthewha1993@gmail.com>
2018-06-18 17:57:45 -0700

    Create error reducer with basic conditions for old state

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,6 +1,11 @@
 import {combineReducers} from 'redux';
 
-const errorsReducer = combineReducers({
-});
+const errorsReducer = (oldState = {}, action) => {
+  Object.freeze(oldState);
+
+  // TODO: Add conditions for errors here.
+  return oldState;
+};
+
 
 export default errorsReducer;
