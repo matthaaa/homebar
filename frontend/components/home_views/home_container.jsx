48a51af29b8a52d4de46eb270da6016e32601d6b
Matt <matthewha1993@gmail.com>
2018-06-19 10:54:46 -0700

    Create home container

---
+++ frontend/components/home_views/home_container.jsx
@@ -0,0 +1,18 @@
+import {connect} from 'react-redux';
+import {withRouter} from 'react-router';
+
+// Components
+import HomeView from './home_view';
+
+
+const mapStateToProps = (state, ownProps) => ({
+})
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+})
+
+
+export default withRouter(connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Component));
