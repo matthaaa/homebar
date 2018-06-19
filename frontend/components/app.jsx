629d8b42faa1103e5a2085d5f90b14a8477b80bd
Matt <matthewha1993@gmail.com>
2018-06-19 10:57:01 -0700

    Create home view and add to app view

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 
 // Components
+import HomeView from './home_views/home_view';
 
 
 const App = ({children}) => (
   <div>
     <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet" />
     <Switch>
-      <Route exact path="/" component={<div></div>} />
+      <Route exact path="/" component={HomeView} />
     </Switch>
   </div>
 );
