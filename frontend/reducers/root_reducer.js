247d8ed14172ef4e0a913497224e82d2594f2da9
Matt <matthewha1993@gmail.com>
2018-06-20 16:55:23 -0700

    Add drinks reducer to root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,10 +1,11 @@
 import {combineReducers} from 'redux';
 import ErrorReducer from './errors_reducer';
+import DrinkReducer from './drink_reducer';
 
 
 const RootReducer = combineReducers({
   errors: ErrorReducer,
-  drinks: DrinksReducer
+  drinks: DrinkReducer
 });
 
 
