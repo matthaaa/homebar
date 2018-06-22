2d4c7b0e4c43a9b04522ae3a56906061eb92c42d
Matt <matthewha1993@gmail.com>
2018-06-22 16:57:54 -0400

    Change fetch drink util method to take in variable for filter

--- frontend/util/drink_api_util.js
+++ frontend/util/drink_api_util.js
@@ -8,9 +8,15 @@ export const fetchDrinks = () => (
   })
 );
 
-export const fetchDrinkByIngredient = (ingredient) => (
+export const fetchDrinkByFilter = (filter, query) => {
+  switch(filter) {
+    case "ingredient":
+      const key = "i";
+  }
+
+  return (
   $.ajax({
     method: "GET",
-    url: `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`
-  })
-);
+    url: `https://www.thecocktaildb.com/api/json/v1/1/filter.php?${key}=${query}`
+  });)
+};
