853e7b475c9ada106692a878c7665d79b6e3dab9
Matt <matthewha1993@gmail.com>
2018-06-24 19:00:39 -0400

    Finish renaming actions in drink actions

--- frontend/reducers/drink_reducer.js
+++ frontend/reducers/drink_reducer.js
@@ -1,11 +1,11 @@
 import {merge} from 'lodash';
-import {RECEIVE_DRINKS_BY_INGREDIENT} from '../actions/drink_actions';
+import {RECEIVE_DRINKS} from '../actions/drink_actions';
 
 const drinkReducer = (oldState = {}, action) => {
   Object.freeze(oldState);
 
   switch(action.type) {
-    case RECEIVE_DRINKS_BY_INGREDIENT:
+    case RECEIVE_DRINKS:
       return merge({}, action.payload.drinks);
     default:
       return oldState;
